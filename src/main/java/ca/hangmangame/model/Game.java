e87c951b7dcfd1f3464e96c82160a799e4c557df
jasmine <jra58@sfu.ca>
2020-08-16 23:44:06 -0700

    set up game class with fields (for game play data) and the relevant accessor/mutator methods

--- src/main/java/ca/hangmangame/model/Game.java
+++ src/main/java/ca/hangmangame/model/Game.java
@@ -1,4 +1,50 @@
 package ca.hangmangame.model;
 
 public class Game {
+    private long id;
+    private String word;
+    private int totalGuesses;
+    private int incorrectGuesses;
+    private GameStatus status;
+    private final int MAX_INCORRECT_GUESSES = 7;
+
+    public Game() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public int getTotalGuesses() {
+        return totalGuesses;
+    }
+
+    public int getIncorrectGuesses() {
+        return incorrectGuesses;
+    }
+
+    public void incrementTotalGuesses(int totalGuesses) {
+        this.totalGuesses += 1;
+    }
+
+    public void incrementIncorrectGuesses(int incorrectGuesses) {
+        this.incorrectGuesses += 1;
+    }
+
+    public GameStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(GameStatus status) {
+        this.status = status;
+    }
 }

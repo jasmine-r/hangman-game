a6a676072e80c3ae8ae259137f6a5f1e9d6bbd0c
jasmine <jra58@sfu.ca>
2020-08-17 00:51:58 -0700

    set up game play webpage format

--- src/main/java/ca/hangmangame/controllers/HangmanController.java
+++ src/main/java/ca/hangmangame/controllers/HangmanController.java
@@ -1,9 +1,11 @@
 package ca.hangmangame.controllers;
 
 import ca.hangmangame.model.Game;
+import ca.hangmangame.model.Guess;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -19,4 +21,18 @@ public class HangmanController {
     public String showWelcomePage(Model model) {
         return "welcome";
     }
+
+    @PostMapping("/game")
+    public String runNewGame(Model model) {
+        // show new game
+        Game currentGame = new Game();
+        currentGame.setId(nextId.incrementAndGet());
+        games.add(currentGame);
+        model.addAttribute("currentGame", currentGame);
+        Guess guess = new Guess();
+        model.addAttribute("guess", guess);
+        return "game";
+    }
+
+
 }
--- src/main/java/ca/hangmangame/model/Game.java
+++ src/main/java/ca/hangmangame/model/Game.java
@@ -1,15 +1,108 @@
 package ca.hangmangame.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Game {
     private long id;
+
     private String word;
+    private WordLoader wordLoader = new WordLoader();
+    private String dashOutput;
+    private String[] dashRepresentation;
+
     private int totalGuesses;
     private int incorrectGuesses;
+    private ArrayList<String> previousGuesses = new ArrayList<>();
     private GameStatus status;
+    private boolean validInputFlag;
     private final int MAX_INCORRECT_GUESSES = 7;
 
     public Game() {
+        wordLoader.loadWordList();
+        setRandomWord(wordLoader.getWordList());
+        setStatus(GameStatus.ACTIVE);
+        this.totalGuesses = 0;
+        this.incorrectGuesses = 0;
+        this.validInputFlag = true;
+    }
 
+    private void setRandomWord(ArrayList<String> words) {
+        if (words.size() != 0) {
+            // get random word from arraylist of words
+            Random random = new Random();
+            this.word = words.get(random.nextInt(words.size()));
+        }
+        // set up dash representation
+        dashRepresentation = new String[word.length()];
+        Arrays.fill(dashRepresentation, "_");
+        this.dashOutput = String.join(" ", dashRepresentation);
+    }
+
+    private void validateInputtedGuess(String guess) {
+        setValidInputFlag(guess.length() > 0 && Character.isLetter(guess.charAt(0)));
+    }
+
+    public void checkGuess(String guess) {
+        validateInputtedGuess(guess);
+
+        if (isValidInputFlag()) {
+            // ensure guess is in lowercase to match the word that is to be guessed
+            guess = guess.toLowerCase();
+
+            if (guess.length() > 1) {
+                guess = guess.substring(0, 1);
+            }
+
+            // confirmed that guess is a single letter (i.e. is valid input & is length 1)
+            if (word.contains(guess)) {
+                // check if this letter has already been guessed
+                if (!previousGuesses.contains(guess)) {
+                    updateDashRepresentation(guess);
+                } // otherwise this letter is in the word, but has already been guessed
+            } else {
+                // the word does not contain the guessed letter
+                incrementIncorrectGuesses();
+            }
+            incrementTotalGuesses();
+            previousGuesses.add(guess);
+
+            // now verify the status of the game
+            verifyGameStatus();
+        }
+    }
+
+    private void verifyGameStatus() {
+        // check for loss
+        if (incorrectGuesses > MAX_INCORRECT_GUESSES) {
+            // reached max incorrect guesses; player has lost
+            setStatus(GameStatus.LOST);
+            return;
+        }
+
+        // check for win
+        String[] wordAsArray = word.split("");
+
+        for (int i = 0; i < wordAsArray.length; i++) {
+            if (!previousGuesses.contains(wordAsArray[i])) {
+                return;
+                // game is still active; player has not won the game at this point in time.
+            }
+        }
+        // player won
+        setStatus(GameStatus.WON);
+    }
+
+    private void updateDashRepresentation(String letterGuessed) {
+        letterGuessed.toCharArray();
+        for (int i = 0; i < word.length(); i++) {
+            Character currentLetter = word.charAt(i);
+            if (currentLetter.compareTo(letterGuessed.charAt(0)) == 0) {
+                dashRepresentation[i] = letterGuessed;
+            }
+        }
+        this.dashOutput = String.join(" ", dashRepresentation);
     }
 
     public long getId() {
@@ -32,11 +125,11 @@ public class Game {
         return incorrectGuesses;
     }
 
-    public void incrementTotalGuesses(int totalGuesses) {
+    public void incrementTotalGuesses() {
         this.totalGuesses += 1;
     }
 
-    public void incrementIncorrectGuesses(int incorrectGuesses) {
+    public void incrementIncorrectGuesses() {
         this.incorrectGuesses += 1;
     }
 
@@ -46,5 +139,17 @@ public class Game {
 
     public void setStatus(GameStatus status) {
         this.status = status;
+    }
+
+    public String getDashOutput() {
+        return dashOutput;
+    }
+
+    public boolean isValidInputFlag() {
+        return validInputFlag;
+    }
+
+    public void setValidInputFlag(boolean validInputFlag) {
+        this.validInputFlag = validInputFlag;
     }
 }

ce29c43cdf64ca7664388aa190141c7a56b844be
jasmine <jra58@sfu.ca>
2020-08-19 18:02:38 -0700

    implement functionality to keep track of gameplay progress

--- src/main/java/ca/hangmangame/controllers/HangmanController.java
+++ src/main/java/ca/hangmangame/controllers/HangmanController.java
@@ -1,11 +1,12 @@
 package ca.hangmangame.controllers;
 
 import ca.hangmangame.model.Game;
+import ca.hangmangame.model.GameStatus;
 import ca.hangmangame.model.Guess;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -32,6 +33,50 @@ public class HangmanController {
         Guess guess = new Guess();
         model.addAttribute("guess", guess);
         return "game";
+    }
+
+    @GetMapping("/game/{id}")
+    public String runExistingGame(@PathVariable("id") long gameId, Model model) {
+        for (Game game : games) {
+            if (game.getId() == gameId) {
+                // set attributes here - need to recall specifics from the game specified
+                model.addAttribute("currentGame", game);
+                Guess guess = new Guess();
+                model.addAttribute("guess", guess);
+
+                if (game.getStatus() == GameStatus.ACTIVE) {
+                    return "game";
+                } else {
+                    return "gameover";
+                }
+            }
+        }
+        throw new GameNotFoundException();
+    }
+
+    @PostMapping("/game/{id}")
+    public String continueGame(@PathVariable("id") long gameId, @ModelAttribute("guess") Guess guess, Model model) {
+        for (Game game : games) {
+            if (game.getId() == gameId) {
+                // retrieve guess and update the specified game
+                game.checkGuess(guess.getGuessedLetter());
+                model.addAttribute("currentGame", game);
+                model.addAttribute("guess", guess);
+
+                if (game.getStatus() == GameStatus.ACTIVE) {
+                    return "game";
+                } else {
+                    return "gameover";
+                }
+            }
+        }
+        throw new GameNotFoundException();
+    }
+
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(GameNotFoundException.class)
+    public String gameNotFoundExceptionHandler(Model model) {
+        return "gamenotfound";
     }
 
 

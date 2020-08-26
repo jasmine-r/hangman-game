d1ba14075fe387a32d6906d37508775b55715334
jasmine <jra58@sfu.ca>
2020-08-25 17:37:23 -0700

    implement class to represent each guess made by user during game

---
+++ src/main/java/ca/hangmangame/model/Guess.java
@@ -0,0 +1,16 @@
+package ca.hangmangame.model;
+
+/**
+ * The Guess class represents the guess inputted by the user.
+ */
+public class Guess {
+    private String guessedLetter;
+
+    public String getGuessedLetter() {
+        return guessedLetter;
+    }
+
+    public void setGuessedLetter(String guessedLetter) {
+        this.guessedLetter = guessedLetter;
+    }
+}
